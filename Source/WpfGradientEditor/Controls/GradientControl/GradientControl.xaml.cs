463a2547aec944ba25a32f1c5ecc8f6fd7800f1f
Victor D. Sandiego <codepuppy@balticsoap.com>
2019-01-30 10:10:52 -0600

    Code cleanup

--- Source/WpfGradientEditor/Controls/GradientControl/GradientControl.xaml.cs
+++ Source/WpfGradientEditor/Controls/GradientControl/GradientControl.xaml.cs
@@ -5,20 +5,11 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Diagnostics;
-using Xceed.Wpf.Toolkit;
 using Xam.Wpf.Controls;
 
 namespace Xam.Applications.GradientEditor.Controls
@@ -332,7 +323,6 @@ namespace Xam.Applications.GradientEditor.Controls
             StopsControl.SliderSet += StopsControlSlidersSet;
             StopsControl.ValueChanged += StopsControlValueChanged;
             StopsControl.SliderSelected += StopsControlSliderSelected;
-            ToolBar b;
         }
         #endregion
 
